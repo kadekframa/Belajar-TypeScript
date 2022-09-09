bb8793079fa1ade796e65c5967d4201b0b45458d
kadekframa <dframa19@gmail.com>
2022-09-09 20:36:32 +0800

    Learning By Doing TypeScript. Tipe data dasar typescript. Semangat!

---
+++ build/typescriptDasar.js
@@ -0,0 +1,25 @@
+"use strict";
+// string.
+let nama = "Kadek Frama";
+nama = "Testing";
+console.info(nama);
+// number.
+let umur = 23;
+umur = 34;
+console.info(umur);
+// boolean.
+let isMarried;
+isMarried = true;
+isMarried = false;
+// isMarried = 2;   // error
+// isMarried = "sudah";   // error
+console.info(isMarried);
+// any.
+let heroes = "Iron First"; // tipe data any pada typescript berarti dapat diisi dengan berbagai value bertipe apapun (dinamis/sama seperti javascript pada umumnya).
+heroes = 20;
+heroes = "New string";
+heroes = true;
+heroes = ["new array"];
+heroes = "new String";
+console.info(heroes);
+//  array.
---
+++ src/typescriptDasar.ts
@@ -0,0 +1,37 @@
+// string.
+let nama: string = "Kadek Frama";
+nama = "Testing";
+
+console.info(nama);
+
+
+// number.
+let umur: number = 23;
+umur = 34;
+
+console.info(umur);
+
+
+// boolean.
+let isMarried: boolean;
+isMarried = true;
+isMarried = false; 
+
+// isMarried = 2;   // error
+// isMarried = "sudah";   // error
+
+console.info(isMarried);
+
+
+// any.
+let heroes: any = "Iron First";     // tipe data any pada typescript berarti dapat diisi dengan berbagai value bertipe apapun (dinamis/sama seperti javascript pada umumnya).
+heroes = 20;
+heroes = "New string";
+heroes = true;
+heroes = ["new array"];
+heroes = "new String";
+
+console.info(heroes);
+
+
+//  array.
