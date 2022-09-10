b4741afe0cd791befc7ee4547168ce2e960a9878
kadekframa <dframa19@gmail.com>
2022-09-10 13:49:23 +0800

    Penggunaan tipe data array pada TypeScript

---
+++ build/array.js
@@ -0,0 +1,15 @@
+"use strict";
+// array.
+let array;
+array = [1, 2, 3];
+let array2;
+array2 = ['string1', 'string2', 'string3'];
+let array3;
+array3 = [1, 'string', true, {}];
+console.info(array3);
+// tuples.
+let biodata;
+biodata = ['Surabaya', 123];
+biodata = ['Badung', 321];
+biodata = ["Karangasem", 100];
+console.info(biodata);
---
+++ src/array.ts
@@ -0,0 +1,19 @@
+// array.
+let array: number[];
+array = [1, 2, 3];
+
+let array2: string[];
+array2 = ['string1', 'string2', 'string3'];
+
+let array3: any[];
+array3 = [1, 'string', true, {}];
+console.info(array3);
+
+
+// tuples.
+let biodata: [string, number];
+biodata = ['Surabaya', 123];
+biodata = ['Badung', 321];
+biodata = ["Karangasem", 100];
+
+console.info(biodata);
